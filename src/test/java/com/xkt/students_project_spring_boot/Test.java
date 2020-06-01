ec13de46457e60200f866d9384c3d68b96e2a816
0913xkt1 <xkt_0913@163.com>
2020-06-01 21:39:53 +0800

    测试github版本控制不太行

--- src/test/java/com/xkt/students_project_spring_boot/Test.java
+++ src/test/java/com/xkt/students_project_spring_boot/Test.java
@@ -1,4 +1,14 @@
 package com.xkt.students_project_spring_boot;
 
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = StudentsProjectSpringBootApplication.class)
 public class Test {
+    @org.junit.jupiter.api.Test
+    public void deleteBathesNotice(){
+        System.out.println("kasjhdaksjdh");
+    }
 }
